805b09188902d3328a26a6556b32a8a4db00191f
miha11-hash <d_mihaela@msn.com>
2024-01-20 22:47:14 +0200

    Error page

--- client/src/pages/Error.jsx
+++ client/src/pages/Error.jsx
@@ -1,15 +1,74 @@
 import React from 'react';
-import {Link, useRouteError} from "react-router-dom";
+import frog from "../assets/frog.jpg";
 import styled from "styled-components";
+import {Link, useRouteError} from "react-router-dom";
 
 function Error() {
     const error=useRouteError();
     console.log(error);
-  return (
-    <div>
-      <h1>Error</h1>
-    </div>
-  );
+
+    if(error.status === 404) {
+        return (
+          <Wrapper style={{overflow: "hidden"}}>
+            <div className="content">
+              <h1>404</h1>
+              <h4>...Oooops! Page not fund!</h4>
+              <img src={frog} alt="cannot found"></img>
+              <Link to="/" className="btn btn-hero">
+                Back Home
+              </Link>
+            </div>
+          </Wrapper>
+        );
+    } else {
+        return <h3>Something goes wrong.</h3>
+    }
 }
+
+const Wrapper = styled.main`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0;
+  padding: 0;
+
+  .content {
+    width: 30rem;
+    height: 30rem;
+  }
+
+  img {
+    width: 20rem;
+    height: 20rem;
+    border-radius: 50%;
+    display: block;
+    margin: 0 auto;
+  }
+
+  .content h1 {
+    font-size: 3.5rem;
+    text-align: center;
+    margin-bottom: 1.5rem;
+    color: var(--green-dark);
+    font-weight: bold;
+  }
+
+  .content h4 {
+    text-align: center;
+    text-transform: none;
+    margin-bottom: 1.5rem;
+    color: var(--green-dark);
+  }
+
+  .content a {
+    display: block;
+    margin: 0 auto;
+    margin-top: 1.5rem;
+    text-align: center;
+    width: 10rem;
+  }
+`;
 
 export default Error;
