01b24dbed3aaf6461024e8bfda821665d50995f7
miha11-hash <d_mihaela@msn.com>
2024-02-29 22:40:48 +0200

    GetAll Jobs

--- server.js
+++ server.js
@@ -6,17 +6,27 @@ import morgan from "morgan";
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+import {nanoid} from "nanoid";
+
+const jobs = [
+    {id: nanoid(), company: "apple", position: "front-end"},
+    {id: nanoid(), company: "microsoft", position: "back-end"}
+]
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  console.log(req);
-  res.send("Hello World!");
+  res.send("Hello world!");
 });
 
 app.post("/", (req, res) => {
   console.log(req);
   res.json({ message: "Data received...", data: req.body });
+});
+
+//get all jobs
+app.get("/api/v1/jobs", (req, res) => {
+  res.status(200).json({ jobs });
 });
 
 const port = process.env.POrt || 3100; 
