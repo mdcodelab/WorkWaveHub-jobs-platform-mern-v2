dd7275e23f53c636b0e4da399a9046e912aa5a9f
miha11-hash <d_mihaela@msn.com>
2024-02-29 23:57:07 +0200

    Get Single Job

--- server.js
+++ server.js
@@ -31,15 +31,27 @@ app.get("/api/v1/jobs", (req, res) => {
 
 //create job
 app.post("/api/v1/jobs", (req, res) => {
-    const {company, position}=req.body;
-    if(!company || !position) {
-        res.status(400).json({msg: "Please provide company && position"});
-    }
-    const id=nanoid(10);
-    const job={id, company, position};
-    jobs.push(job);
-  res.status(200).json({ job });
+  const { company, position } = req.body;
+  if (!company || !position) {
+    return res.status(400).json({ msg: "Please provide company && position" });
+  }
+  const id = nanoid(10);
+  const job = { id, company, position };
+  jobs.push(job);
+  res.status(201).json({ job }); //when create a resource, status=201
 });
+
+//get single job
+app.get("/api/v1/jobs/:id", (req, res) => {
+    const {id}=req.params;
+    const job=jobs.find((job) => job.id === id);
+    if(!job) {
+        res.status(404).json({msg: `No job with id ${id}`})
+    }
+  res.status(201).json({ job }); 
+});
+
+
 
 
 
