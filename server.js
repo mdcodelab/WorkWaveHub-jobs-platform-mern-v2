c1488d64ee5e14de4b06dadd65ed947cedd5cf57
miha11-hash <d_mihaela@msn.com>
2024-02-29 20:36:48 +0200

    dotenv & morgen

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
-import express from "express"
+import * as dotenv from "dotenv";
+dotenv.config();
+import express from "express";
 const app = express();
+import morgan from "morgan";
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Hello");
+  console.log(req);
+  res.send("Hello World!");
 });
 
-app.listen(3001, () => {
-    console.log("server is running...")
-})
+app.post("/", (req, res) => {
+  console.log(req);
+  res.json({ message: "Data received...", data: req.body });
+});
+
+const port = process.env.POrt || 3100; 
+
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
