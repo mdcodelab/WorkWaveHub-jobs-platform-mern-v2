8840cfc6f96ebd393d5ae47fae90b867f440f4db
miha11-hash <d_mihaela@msn.com>
2024-03-06 15:46:50 +0200

    hash password

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,25 +1,33 @@
 import User from "../models/userModel.js";
 import { StatusCodes } from "http-status-codes";
+import bcrypt from "bcrypt";
 
 
 //create user
-// export const register = async (req, res) => {
-//     try {
-//         const user = await User.create(req.body);
-//         const isFirstAccount = (await User.countDocuments()) === 1 
-//         req.body.role = isFirstAccount ? "admin" : "user";
-//     res.status(StatusCodes.CREATED).json({user});
-//     } catch (error) {
-//       res.status(500).json({error: "Internal server error"})      
-//     }
-// }
 
 export const register = async (req, res) => {
   try {
-    const user = await User.create(req.body);
-    const isFirstAccount = (await User.countDocuments()) === 1; // if the first user registered
+    const { name, email, password, lastName, location } = req.body;
+
+    // Criptarea parolei
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Crearea utilizatorului cu parola criptată
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword, // Salvăm parola criptată
+      lastName,
+      location,
+    });
+
+    // Verificarea dacă acesta este primul cont înregistrat și setarea rolului
+    const isFirstAccount = (await User.countDocuments()) === 1;
     user.role = isFirstAccount ? "admin" : "user";
-    await user.save(); 
+
+    // Salvarea modificărilor în baza de date
+    await user.save();
 
     res.status(StatusCodes.CREATED).json({ user });
   } catch (error) {
@@ -28,6 +36,7 @@ export const register = async (req, res) => {
       .json({ error: "Internal server error" });
   }
 };
+
 
 
 
