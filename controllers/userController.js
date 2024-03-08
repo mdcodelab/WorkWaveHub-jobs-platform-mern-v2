f8be331e9c5987f1bbf30d62c20480c4f78147db
miha11-hash <d_mihaela@msn.com>
2024-03-08 07:38:00 +0200

    jwt

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 import User from "../models/userModel.js";
 import { StatusCodes } from "http-status-codes";
 import bcrypt from "bcrypt";
+import { createJWT } from "../utils/jwtToke.js";
 
 
 //create user
@@ -25,38 +26,8 @@ export const register = async (req, res) => {
 }
 
 
-// export const login = async (req, res) => {
-//   try {
-//     const { email, password } = req.body;
-
-//     // Găsim utilizatorul cu adresa de email specificată
-//     const user = await User.findOne({ email });
-
-//     // Verificăm dacă utilizatorul a fost găsit
-//     if (!user) {
-//       return res.status(401).json({ msg: "User not found" });
-//     }
-
-//     // Comparam parola trimisă cu parola hash a utilizatorului
-//     const isPasswordCorrect = await bcrypt.compare(password, user.password);
-
-//     // Verificăm dacă parola este corectă
-//     if (!isPasswordCorrect) {
-//       return res.status(401).json({ msg: "Wrong password" });
-//     }
-
-//     // Răspunsul dacă autentificarea este reușită
-//     res.status(StatusCodes.OK).json({ msg: "User logged in" });
-//   } catch (error) {
-//     // Tratarea erorilor interne
-//     res
-//       .status(StatusCodes.INTERNAL_SERVER_ERROR)
-//       .json({ msg: "Internal server error" });
-//   }
-// };
-
 export const login = async (req, res) => {
-  try {
+  
     const { email, password } = req.body;
 
     // Găsim utilizatorul cu adresa de email specificată
@@ -72,14 +43,11 @@ export const login = async (req, res) => {
       return res.status(401).json({ msg: "Wrong password" });
     }
 
+    const token=createJWT({userID: user._id, role: user.role});
+
     // Răspunsul dacă autentificarea este reușită
-    res.status(StatusCodes.OK).json({ msg: "User logged in" });
-  } catch (error) {
-    // Tratarea erorilor interne
-    res
-      .status(StatusCodes.INTERNAL_SERVER_ERROR)
-      .json({ msg: "Internal server error" });
-  }
+    res.status(StatusCodes.OK).json({ token });
+  
 };
 
 
