2f86e53c4144144e8ab673a2d853298ca96b47da
miha11-hash <d_mihaela@msn.com>
2024-03-14 13:06:07 +0200

    Login - React Action, useNavigation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,6 +16,7 @@ import {
   Admin,
 } from "./pages";
 import { action as registerAction } from "./pages/Register";
+import {action as loginAction} from "./pages/Login";
 
 const router = createBrowserRouter([
   {
@@ -29,7 +30,7 @@ const router = createBrowserRouter([
         element: <Register />,
         action: registerAction,
       },
-      { path: "login", element: <Login></Login> },
+      { path: "login", element: <Login></Login>, action: loginAction },
       {
         path: "dashboard",
         element: <Dashboard></Dashboard>,
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,63 +1,37 @@
 import React from 'react';
-import {Link, useNavigate} from "react-router-dom";
+import { Link, useNavigation, Form, redirect} from "react-router-dom";
+import FormRow from "../components/FormRow";
 import styled from "styled-components";
-import Logo from '../components/Logo';
+import Logo from "../components/Logo";
 import { toast } from "react-toastify";
 import axios from "axios";
 
+export const action = async ({request}) => {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+  try {
+    await axios.post("/api/v1/auth/login", data);
+    toast.success("Login successful");
+    return redirect("/dashboard");
+  } catch (error) {
+    toast.error(
+      `Error: ${error.response.data.message || error.response.statusText}`);
+    return error;
+  }
+}
 
 function Login() {
-  const[formData, setFormData]=React.useState({
-    email: "",
-    password: ""
-  });
-
- 
-  function handleChange(e) {
-    const { name, value } = e.target;
-    setFormData((prevState) => ({
-      ...prevState, [name]: value,
-    }));
-  }
-const navigate=useNavigate();
-  const[isSubmitting, setIsSubmitting]=React.useState(false);
-
-async function handleSubmit(e) {
-e.preventDefault();
-setIsSubmitting(true);
-try {
-  const response = await axios.post("/api/v1/auth/login", formData);
-  console.log(response.data);
-  toast.success("Registration successful");
-  navigate("/dashboard");
-} catch (error) {
-  toast.error(`Error: ${error.response.data.message || error.response.statusText}`);
-  console.error(error);
-}
-finally {
-setIsSubmitting(false);
-}
-}
+  const navigation=useNavigation();
+  const isSubmitting = navigation.state === "submitting";
+  console.log(isSubmitting);
 
   return (
     <Wrapper>
-      <form className="form" onSubmit={handleSubmit}>
-        <div className="logo">
-          <Logo></Logo>
-        </div>
-        <h3>Login</h3>
-        <div className="form-row">
-          <label htmlFor="email">Email</label>
-          <input type="email" name="email" id="email" value={formData.email} 
-          onChange={handleChange} required></input>
-        </div>
-
-        <div className="form-row">
-          <label htmlFor="password">Password</label>
-          <input type="password" name="password" id="password" value={formData.password}
-          onChange={handleChange} required></input>
-        </div>
-
+      <Form method="post" className="form">
+        <Logo />
+        <h4>login</h4>
+        <FormRow type="email" name="email" />
+        <FormRow type="password" name="password" />
         <button type="submit" className="btn btn-block" disabled={isSubmitting}>
           {isSubmitting ? "Submitting..." : "Submit"}
         </button>
@@ -66,102 +40,59 @@ setIsSubmitting(false);
         </button>
         <div className="check-member">
           <span>Not a member?</span>
-          <Link
-            to="/register"
-            className="member-btn"
-            style={{ color: "var(--primary-700" }}
-          >
-            Register
-          </Link>
+          <Link to="/register" className="member-btn" style={{ color: "var(--primary-700" }}>
+            Register</Link>
         </div>
         <Link to="/" className="back-home">
           Back Home
         </Link>
-      </form>
+      </Form>
     </Wrapper>
   );
 }
 
-const Wrapper = styled.div`
-  height: 100vh;
-  width: 100%;
-  display: flex;
+const Wrapper = styled.section`
+  min-height: 100vh;
+  display: grid;
   align-items: center;
-  justify-content: center;
-
-  .form {
-    width: 300px;
-    height: max-content;
-    box-shadow: var(--shadow-2);
-    border-top: 4px solid var(--primary-500);
-  }
-
   .logo {
-    width: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
-
-  .form h3 {
-    margin: 1rem 0;
-    text-align: center;
-  }
-
-  .check-member {
-    display: flex;
-    width: 100%;
-    align-items: center;
-    justify-content: center;
-    margin: 1.5rem 0;
-  }
-
-  .member-btn {
     display: block;
-    margin-left: 1rem;
+    margin: 0 auto;
+    margin-bottom: 1.38rem;
   }
-
+  .form {
+    max-width: 400px;
+    margin: 0 auto;
+    border-top: 5px solid var(--primary-500);
+  }
+  h4 {
+    text-align: center;
+    margin-bottom: 1.38rem;
+  }
+  p {
+    margin-top: 1rem;
+    text-align: center;
+    line-height: 1.5;
+  }
+  .btn {
+    margin-top: 1rem;
+  }
+  .member-btn {
+    color: var(--primary-500);
+    letter-spacing: var(--letter-spacing);
+    margin-left: 0.25rem; 
+  }
+  .check-member {
+    width: max-content;
+    margin: 0 auto;
+    margin-top: 1.5rem;
+  }
   .back-home {
     display: block;
-    margin: 0 auto;
     text-align: center;
+    margin-top: 1.5rem;
+    color: blue;
   }
-
-  .explore {
-    display: block;
-    margin: 1.5rem 0;
-  }
-  .explore a {
-    color: #fff;
-  }
-
-  .form-row {
-    width: 98%;
-    margin: 0 auto;
-    margin-bottom: 1rem;
-    display: flex;
-    flex-direction: column;
-  }
-
-  .form-row label {
-    display: block;
-    margin-bottom: 0.5rem;
-  }
-
-  .form-row input {
-    width: 100%;
-    padding: 0.2rem 0;
-    border-radius: 0.25rem;
-    border: 1.5px solid grey;
-    outline: none;
-    background: transparent;
-    color: grey;
-    transition: all 0.3s;
-  }
-
-  .form-row input:focus {
-    border: 3px solid var(--primary-500);
-  }
-`;
+  `
 
 export default Login;
