f362aa90a52d117c5be9f9dd9ad079475fdb31c7
miha11-hash <d_mihaela@msn.com>
2024-03-23 16:47:39 +0200

    search params: jobType & jobSearch

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -14,7 +14,29 @@ import dayjs from "dayjs";
 // }
 
 export const getAllJobs = async (req, res) => {
-  const jobs = await Job.find({ createdBy: req.user.userId});
+  const {search, jobStatus, jobType}=req.query;
+  let queryObject={
+    createdBy: req.user.userId
+  }
+  
+  if (search) {
+    queryObject.$or = [
+      { position: { $regex: search, $options: 'i' } },
+      { company: { $regex: search, $options: 'i' } },
+    ];
+  }
+
+  if(jobStatus && jobStatus !== "all") {
+    queryObject.jobStatus = jobStatus;
+  }
+
+  if (jobType && jobType !== "all") {
+    queryObject.jobType = jobType;
+  }
+
+  console.log(req.query);
+  //console.log(search);
+  const jobs = await Job.find(queryObject);
   res.status(StatusCodes.OK).json({ jobs });
 };
 
