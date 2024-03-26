76089155bf4daf4b0d831a2e3961a891ba7fa5c2
miha11-hash <d_mihaela@msn.com>
2024-03-26 11:09:02 +0200

    render buttons

--- client/src/components/PageContainer.jsx
+++ client/src/components/PageContainer.jsx
@@ -9,12 +9,28 @@ function PageContainer() {
     const {data}=useAllJobsContext();
     const {numberOfPages, currentPage}=data;
     console.log(numberOfPages, currentPage);
+    const pages = [];
+    for(let i =1; i<= numberOfPages; i++) {
+        pages.push(i);
+    }
+    console.log(pages);
 
   return (
     <div>
-      <h1>Page Container</h1>
+      <button className="btn prev-btn">
+        <HiChevronDoubleLeft></HiChevronDoubleLeft> Prev
+      </button>
+      <div className="btn-container">
+        {pages.map((pageNumber)=> {
+            return <button className={`btn page-btn ${pageNumber === currentPage? "active" : ""}`} 
+            key={pageNumber}>{pageNumber}</button>
+        })}
+      </div>
+      <button className="btn next-btn">
+        Next <HiChevronDoubleRight></HiChevronDoubleRight>
+      </button>
     </div>
-  )
+  );
 }
 
 export default PageContainer
